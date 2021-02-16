b9ef1be3abdcd20e09cc142fcb81f157223551c2
CamiloFajardo200 <camilo.fajardo.2001@hotmail.com>
2021-02-16 14:20:35 +0300

    HW by Camilo Fajardo

---
+++ counted.h
@@ -0,0 +1,24 @@
+#ifndef COUNTED_H
+#define COUNTED_H
+#include<string>
+#include<vector>
+
+
+class counted
+{
+private:
+  static std::vector<int> used_id;
+  int id;
+  void SetId(int number);
+  static int Number;
+  static int LastNumber;
+
+public:
+  int getid();
+    counted();
+  counted(const counted&);
+  counted& operator=(const counted&);
+  ~counted();
+};
+
+#endif // COUNTED_H
