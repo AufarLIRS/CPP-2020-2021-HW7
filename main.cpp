bc3555ce9656a971f2a73650cc2da833d47f19e8
Safarova-905_2 <liaisansafarova@gmail.com>
2021-03-01 22:49:49 +0300

    hw

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include "task1.h"
 
 using namespace std;
 
+int Counted::uniq = 0;
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    Counted counted1;
+    Counted counted2;
+    Counted counted3;
+    Counted* counted4 = new Counted();
+    delete counted4;
+    Counted counted5;
     return 0;
 }
